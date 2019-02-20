e97ab324591c649c3112e105946d7a1fd865bdaa
Erdostal <erdostal@iastate.edu>
2019-02-20 14:02:03 -0600

    Added list templates for all models, attempted detail view

--- isu_lims/lims/urls.py
+++ isu_lims/lims/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
+from .views import GenusDetail
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('genera', views.genera, name='genera'),
     path('subgenera', views.subgenera, name='subgenera'),
+    path('genome', views.genome, name='genome'),
+    path('species', views.species, name='species'),
+    path('accession', views.accession, name='accession'),
+    path('plant', views.plant, name='plant'),
+    path('seedpacket', views.seedpacket, name='seedpacket'),
+    path('seed', views.seed, name='seed'),
+    path('project', views.project, name='project'),
+    path('sample', views.sample, name='sample'),
+    path('genera/<genus>/', GenusDetail.as_view(), name='genus-detail'),
 ]
--- isu_lims/lims/views.py
+++ isu_lims/lims/views.py
@@ -2,14 +2,15 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 from iastatetheme.models import SiteTheme
+from django.views.generic.detail import DetailView
 
-from .models import Genus
 
 
-from .models import Genus
 
 def index(request):
     return render(request, 'lims/index.html')
+
+from .models import Genus
 
 def genera(request):
     genera_list = Genus.objects.all()
@@ -17,6 +18,21 @@ def genera(request):
     }
 
     return render(request, 'genera/index.html', context)
+
+from django.views.generic import DetailView
+from .models import Genus, Subgenus
+
+class GenusDetail(DetailView):
+
+    model = Genus
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['genera_list'] = Genus.objects.all()
+        return context
+
 
 from .models import Subgenus
 
@@ -27,3 +43,76 @@ def subgenera(request):
 
     return render(request, 'subgenera/index.html', context)
 
+from .models import Genome
+
+def genome(request):
+    genome_list = Genome.objects.all()
+    context = {'genome_list': genome_list,
+    }
+
+    return render(request, 'genome/index.html', context)
+
+
+from .models import Species
+
+def species(request):
+    species_list = Species.objects.all()
+    context = {'species_list': species_list,
+    }
+
+    return render(request, 'species/index.html', context)
+
+from .models import Accession
+
+def accession(request):
+    accession_list = Accession.objects.all()
+    context = {'accession_list': accession_list,
+    }
+
+    return render(request, 'accession/index.html', context)
+
+from .models import Plant
+
+def plant(request):
+    plant_list = Plant.objects.all()
+    context = {'plant_list': plant_list,
+    }
+
+    return render(request, 'plant/index.html', context)
+
+from .models import SeedPacket
+
+def seedpacket(request):
+    seedpacket_list = SeedPacket.objects.all()
+    context = {'seedpacket_list': seedpacket_list,
+    }
+
+    return render(request, 'seedpacket/index.html', context)
+
+from .models import Seed
+
+def seed(request):
+    seed_list = Seed.objects.all()
+    context = {'seed_list': seed_list,
+    }
+
+    return render(request, 'seed/index.html', context)
+
+from .models import Project
+
+def project(request):
+    project_list = Project.objects.all()
+    context = {'project_list': project_list,
+    }
+
+    return render(request, 'project/index.html', context)
+
+from .models import Sample
+
+def sample(request):
+    sample_list = Sample.objects.all()
+    context = {'sample_list': sample_list,
+    }
+
+    return render(request, 'sample/index.html', context)
+    
