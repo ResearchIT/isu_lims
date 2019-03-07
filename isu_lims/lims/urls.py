2394e4878db25c20c2873de7676aa789d55ad387
Erdostal <erdostal@iastate.edu>
2019-03-07 15:48:49 -0600

    3.7.2019 adding detail views

--- isu_lims/lims/urls.py
+++ isu_lims/lims/urls.py
@@ -16,6 +16,17 @@ urlpatterns = [
     path('project', views.project, name='project'),
     path('sample', views.sample, name='sample'),
     path('name', views.name, name='name'),
-    path('thanks', views.thanks, name='thanks')
+    path('thanks', views.thanks, name='thanks'),
     path('genus/<int:pk>/', GenusDetailView.as_view(), name='genus-detail'),
+    path('subgenus/<int:pk>/', SubenusDetailView.as_view(), name='subgenus-detail'),
+    path('genome/<int:pk>/', GenomeDetailView.as_view(), name='genome-detail'),
+    path('species/<int:pk>/', SpeciesDetailView.as_view(), name='species-detail'),
+    path('accession/<int:pk>/', AccessionDetailView.as_view(), name='accession-detail'),
+    path('plant/<int:pk>/', PlantDetailView.as_view(), name='plant-detail'),
+    path('seedpacket/<int:pk>/', SeedPacketDetailView.as_view(), name='seedpacket-detail'),
+    path('seed/<int:pk>/', SeedDetailView.as_view(), name='seed-detail'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('sample/<int:pk>/', SampleDetailView.as_view(), name='sample-detail'),
+
+
 ]
