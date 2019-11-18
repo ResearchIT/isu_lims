0e852344bccf336f65191aaea045b0c70b38498d
Nick Booher <njbooher@iastate.edu>
2019-11-18 15:55:08 -0600

    Fix field naems

--- lims/filters.py
+++ lims/filters.py
@@ -3,9 +3,9 @@ from . import models
 
 class AccessionsFilter(django_filters.FilterSet):
     accession = django_filters.CharFilter(field_name='accession', label='Search plant name', lookup_expr='contains')
-    genus = django_filters.ModelChoiceFilter(field_name='species.genome.subgenus.genus', queryset=models.Genus.objects.all())
-    subgenus = django_filters.ModelChoiceFilter(field_name='species.genome.subgenus', queryset=models.Subgenus.objects.all())
-    genome = django_filters.ModelChoiceFilter(field_name='species.genome', queryset=models.Genome.objects.all())
+    genus = django_filters.ModelChoiceFilter(field_name='species__genome__subgenus__genus', queryset=models.Genus.objects.all())
+    subgenus = django_filters.ModelChoiceFilter(field_name='species__genome__subgenus', queryset=models.Subgenus.objects.all())
+    genome = django_filters.ModelChoiceFilter(field_name='species__genome', queryset=models.Genome.objects.all())
     species = django_filters.ModelChoiceFilter(field_name='species', queryset=models.Species.objects.all())
     alternatenames = django_filters.CharFilter(field_name='alternatenames', label='Search alternate names', lookup_expr='contains')
     class Meta:
