718048205ac5e7088a9afccfeeb3ed604ea8bc68
Nick Booher <njbooher@iastate.edu>
2020-02-26 14:29:49 -0600

    Update labels to use flat fields

--- lims/models.py
+++ lims/models.py
@@ -90,7 +90,7 @@ class Plant(models.Model):
     flowering = models.BooleanField(null = True)
 
     def __str__(self):
-        return self.accession.species.genome.subgenus.genus.genus + ' ' + self.accession.species.species + ' ' + self.accession.accession
+        return self.accession.genusflat + ' ' + self.accession.speciesflat + ' ' + self.accession.accession
 
 # need to track who has them checked out / location (box, or desk, or storage, etc.)
 # should be in storage, but often kept at workbench
@@ -108,7 +108,7 @@ class SeedPacket(models.Model):
     checkedoutby = models.ForeignKey(User, on_delete=models.CASCADE, blank = True, null = True)
 
     def __str__(self):
-        return self.accession.species.genome.subgenus.genus.genus + ' ' + self.accession.species.species + ' ' + self.accession.accession
+        return self.accession.genusflat + ' ' + self.accession.speciesflat + ' ' + self.accession.accession
 
 class Sample(models.Model):
 
