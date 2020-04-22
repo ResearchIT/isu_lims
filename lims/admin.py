56ea86787f1442646b6fb0fe3e703a0bcd1974b0
Nick Booher <njbooher@iastate.edu>
2020-04-22 16:57:09 -0500

    Show more stuff on admin page

--- lims/admin.py
+++ lims/admin.py
@@ -38,3 +38,4 @@ class SampleAdmin(admin.ModelAdmin):
 @admin.register(models.SeedPacket)
 class SeedPacketAdmin(admin.ModelAdmin):
     autocomplete_fields = ['accession']
+    list_display = ['accession', 'parenta', 'parentb', 'quantity', 'datecollected', 'location', 'checkedoutby', 'notes']
