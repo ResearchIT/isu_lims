216eeb68f66a6fa9d675e536ef8955d394300caa
Nick Booher <njbooher@iastate.edu>
2020-09-16 10:58:56 -0500

    Upgrade everything

--- lims/settings.py
+++ lims/settings.py
@@ -163,8 +163,10 @@ else:
     AWS_S3_REGION_NAME = os.getenv('LIMS_CLOUD_REGION_NAME')
     AWS_S3_ENDPOINT_URL = os.getenv('LIMS_CLOUD_ENDPOINT_URL')
     AWS_S3_ADDRESSING_STYLE = "path"
-    AWS_DEFAULT_ACL = None
     AWS_S3_FILE_OVERWRITE = False
+    AWS_S3_OBJECT_PARAMETERS = {
+        'ACL': 'private'
+    }
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     # Logging
