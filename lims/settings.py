7399b733683b9dd1fc3f308f491bcc9471d7a697
Nick Booher <njbooher@iastate.edu>
2021-05-04 14:30:55 -0500

    Add new setting

--- lims/settings.py
+++ lims/settings.py
@@ -118,6 +118,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 if os.getenv('LIMS_DEVEL_ACTIVE', '') == 'IM_REALLY_SURE_THIS_ISNT_PRODUCTION':
 
     # SECURITY WARNING: don't run with debug turned on in production!
