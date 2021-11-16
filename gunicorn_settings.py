5a30cc4e40da84344adf3a7d716e290b492fe12e
Nick Booher <njbooher@iastate.edu>
2021-11-16 15:43:20 -0600

    update for newer python container

--- gunicorn_settings.py
+++ gunicorn_settings.py
@@ -1,5 +1,8 @@
 import os
 
+accesslog = '-'
+bind = ['0.0.0.0:8080']
+
 keyfile = os.getenv('LIMS_SSL_KEYFILE')
 certfile = os.getenv('LIMS_SSL_CERTFILE')
 ca_certs = os.getenv('LIMS_SSL_CA_CERTS')
